1dec5c62025edb9cc4571d2ba20d9a371eeb1dff
KevinH <khui98@gmail.com>
2023-11-13 14:01:21 -0500

    Added email functionality
    Recipient gets email based on what they submitted in the online form.

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+app.config["SECRET_KEY"] = "myapplication123"
+# Changes directory of where data.db is stored and used from
+# app.root_path is absolute path to current python app directory
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{app.root_path}/instance/data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "khui3850@gmail.com"
+app.config["MAIL_PASSWORD"] = os.getenv("PASSWORD")
+
+db = SQLAlchemy(app)
+
+mail = Mail(app)
+
+
+class Form(db.Model):
+    """Creates columns their datatypes inside data.db"""
+    id_num = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name, 
+                    email=email, date=date_obj, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}.\n" \
+                        f"Here is your data:\n{first_name}\n{last_name}\n{date}\n" \
+                            f"Thank you!"
+        message = Message("New form submission", sender=app.config["MAIL_USERNAME"], recipients=[email], body=message_body)
+        mail.send(message)
+
+        flash(f"{first_name}, your form was submitted successfully!", "success")
+
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
